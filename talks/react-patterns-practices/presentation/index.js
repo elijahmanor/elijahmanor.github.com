e2d278d040371b3dff55898d3e442d8057d5d702
Elijah Manor <elijah.manor@gmail.com>
2017-05-20 13:17:39 -0500

    add more code samples to react-pnp

--- talks/react-patterns-practices/presentation/index.js
+++ talks/react-patterns-practices/presentation/index.js
@@ -16,7 +16,12 @@ import {
   ComponentPlayground,
   Fill,
   Layout,
-  MarkdownSlides
+  MarkdownSlides,
+  Link,
+  Markdown,
+  SlideSet,
+  Appear,
+  Interactive
 } from "spectacle";
 
 // Import image preloader util
@@ -59,7 +64,7 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
-        <Slide bgColor="primary" bgImage={images.react} bgDarken={0.8}>
+        <Slide bgColor="primary" bgImage={images.react.replace("/", "")} bgDarken={0.8}>
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
             React
           </Heading>
@@ -70,9 +75,76 @@ export default class Presentation extends React.Component {
             by @elijahmanor
           </Text>
         </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="quartenary">
+          <Heading size={2} caps textColor="primary">
+            Agenda
+          </Heading>
+          <List>
+            <Appear><ListItem>Refactoring a Component</ListItem></Appear>
+            <Appear><ListItem>Unit Testing Components</ListItem></Appear>
+            <Appear><ListItem>Creating a Pattern Library</ListItem></Appear>
+          </List>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="quartenary">
+          <Heading size={2} caps fit textColor="primary">
+            Refactoring a Component
+          </Heading>
+          <List>
+            <Appear><ListItem>Container &amp; Presentational Components</ListItem></Appear>
+            <Appear><ListItem>Stateless Functional Components</ListItem></Appear>
+            <Appear><ListItem>Higher Order Components</ListItem></Appear>
+            <Appear><ListItem>Accessing Performance Bottlenecks</ListItem></Appear>
+            <Appear><ListItem>Introducting Immutability</ListItem></Appear>
+            <Appear><ListItem>Adding `redux`</ListItem></Appear>
+            <Appear><ListItem>Adding `react-router`</ListItem></Appear>
+            <Appear><ListItem>Adding `recompose`</ListItem></Appear>
+          </List>
+        </Slide>
+        <Slide>
+          <Markdown>
+            {`
+  ![Markdown Logo](${images.markdown.replace("/", "")})
+  You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
+  * Lists too!
+  * With ~~strikethrough~~ and _italic_
+  * And let's not forget **bold**
+            `}
+          </Markdown>
+        </Slide>
+        {
+          MarkdownSlides`
+#### Create Multiple Slides in Markdown
+All the same tags and elements supported in <Markdown /> are supported in MarkdownSlides.
+---
+Slides are separated with **three dashes** and can be used _anywhere_ in the deck. The markdown can either be:
+* A Tagged Template Literal
+* Imported Markdown from another file
+          `
+        }
+        { /*<SlideSet>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <List>
+              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
+              <Appear><ListItem>Autofit text</ListItem></Appear>
+              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
+              <Appear><ListItem>PDF export</ListItem></Appear>
+              <Appear><ListItem>And...</ListItem></Appear>
+            </List>
+          </Slide>
+          <Slide transition={["slide"]} bgColor="primary">
+            <Heading size={1} caps fit textColor="tertiary">
+              Your presentations are interactive
+            </Heading>
+            <Interactive/>
+          </Slide>
+        </SlideSet> */ }
         <Slide bgColor="secondary">
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
-            Simple React Component
+            Simple React
+          </Heading>
+          <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
+            Component
           </Heading>
         </Slide>
         <Slide maxHeight="100vh" maxWidth="90vw">
@@ -82,6 +154,7 @@ export default class Presentation extends React.Component {
           <Jokes />
         </Slide>
         <CodeSlide
+          maxHeight="100vh" maxWidth="75vw"
           transition={[]}
           lang="js"
           code={require("raw-loader!../assets/jokes-1.example")}
@@ -165,6 +238,12 @@ export default class Presentation extends React.Component {
             { loc: [2, 3] },
             { loc: [8, 10] },
             ]} />
+        <Slide transition={["fade"]} bgColor="black">
+          <BlockQuote>
+            <Quote>My favorite part of React is what I loved about MooTools: to use it effectively you learn JavaScript, not a DSL: useful your whole career.</Quote>
+            <Cite><Link href="https://twitter.com/ryanflorence/status/577685415919898625">Ryan Florence </Link></Cite>
+          </BlockQuote>
+        </Slide>
         <Slide maxHeight="100vh" maxWidth="90vw">
           <div>
             <a href="https://codesandbox.io/s/nZz6p1WDl">
@@ -188,7 +267,7 @@ export default class Presentation extends React.Component {
         <CodeSlide
           transition={[]}
           lang="js"
-          code={require("raw-loader!../assets/jokes-9.example")}
+          code={require("raw-loader!../assets/jokes-9-a.example")}
           ranges={[
             { loc: [0, 270], title: "Walking through some code" },
             { loc: [0, 1], title: "The Beginning" },
@@ -239,14 +318,14 @@ Another Test
         ` */ }
         <Slide bgColor="primary" bgImage={images.react} bgDarken={0.8}>
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
-            Thanks You!
+            Thank You!
           </Heading>
           <Heading size={1} fit caps lineHeight={1} textColor="tertiary">
             Slides <a href="http://bit.ly/react-pnp">http://bit.ly/react-pnp</a>
           </Heading>
           <Text margin="10px 0 0" textColor="quartenary" bold>
-            <a href="http://elijahmanor.com">http://elijahmanor.com</a>
-            @elijahmanor
+            <a href="http://elijahmanor.com" style={{ display: "block" }}>http://elijahmanor.com</a>
+            <a href="http://twitter.com/elijahmanor" style={{ display: "block" }}>@elijahmanor</a>
           </Text>
         </Slide>
       </Deck>
