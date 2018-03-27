6ddfa874635e4bd15f6151bbe13c612b19d043f4
Elijah Manor <elijah.manor@gmail.com>
2018-03-27 13:01:50 -0400

    Fix the annotated flag mode for react-16-3 slides

--- talks/react-16-3/js/custom.js
+++ talks/react-16-3/js/custom.js
@@ -1,11 +1,15 @@
 function handleSlideChanged( event ) {
+  if ( window.localStorage.isAnnotated === undefined ) {
+    console.log( 'isAnnotated === undefined' );
+    window.localStorage.isAnnotated = true;
+  }
   event = event || { currentSlide: { dataset: {} } };
   var header = document.querySelector('.Title');
   var title = event.currentSlide.dataset.title || '';
   var state = event.currentSlide.dataset.state || '';
   var isRecording = window.localStorage.isRecording === 'true';
   var isAnnotated = window.localStorage.isAnnotated === "true";
-
+ 
   header.classList.toggle('Title--show', !!title);
   document.body.classList.toggle('is-recording', isRecording);
   document.body.classList.toggle('is-annotated', isAnnotated);
